948837bd0208e1b3b560230614eb9eecd641f42f
XianQijing <154122662@qq.com>
2018-12-19 17:34:28 +0800

    'sss'

--- src/components/pages/StageManagement.vue
+++ src/components/pages/StageManagement.vue
@@ -1,7 +1,110 @@
 <template>
-  <!-- 分期管理 -->
+  <!-- 保单列表 -->
   <div class="StageManagement">
-    StageManagement
+
+    <div class="table-search">
+      <el-select v-model="value" clearable placeholder="请选择">
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+        </el-option>
+      </el-select>
+      <el-input
+        placeholder="请输入内容"
+        v-model="value"
+        clearable
+        class="input">
+      </el-input>
+      <button class="table-btn-search">查询</button>
+      <button class="table-btn-clear">清空</button>
+    </div>
+
+    <div class="delete">
+      删除
+    </div>
+
+    <div class="ower-table">
+      <el-table
+        ref="multipleTable"
+        :data="tableData"
+        style="width: 100%"
+        @selection-change="handleSelectionChange"
+        height="580">
+        <el-table-column
+          type="selection"
+          width="55">
+        </el-table-column>
+        <el-table-column
+          prop="date"
+          label="订单号">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="保单号">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="姓名">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="手机号">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="车牌">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="生效日期">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="保费合计">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="保单状态">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="支付状态">
+        </el-table-column>
+        <el-table-column
+          label="操作">
+          <template slot-scope="scope">
+            <el-button type="text" style="color: #5962FF;" @click="openDia">查看详情</el-button>
+            <el-button type="text" style="color: red;">去支付</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+
+    <div class="ower-pages" v-if="pages.total > pages.pageSize">
+      <el-pagination
+        background
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="pages.currentPage"
+        :page-sizes="pages.pageSizes"
+        :page-size="pages.pageSize"
+        layout="sizes, prev, pager, next, total"
+        :total="pages.total">
+      </el-pagination>
+    </div>
+
+    <el-dialog
+      :visible.sync="dialogVisible"
+      width="30%">
+      <div class="dialog-header">详情</div>
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -10,11 +113,50 @@ export default {
   name: 'StageManagement',
   data () {
     return {
+      value: '',
+      options: [],
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }
+      ],
+      pages: {
+        currentPage: 1,
+        pageSize: 10,
+        pageSizes: [10, 20, 30, 40],
+        total: 10
+      },
+      multipleSelection: [],
+      dialogVisible: false
+    }
+  },
+  mounted () {
+    this.pages.total = this.tableData.length
+  },
+  methods: {
+    handleSizeChange (val) {
+      console.log(`每页 ${val} 条`)
+    },
+    handleCurrentChange (val) {
+      console.log(`当前页: ${val}`)
+    },
+    handleSelectionChange (val) {
+      this.multipleSelection = val
+    },
+    openDia () {
+      this.dialogVisible = true
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-.StageManagement {}
+.StageManagement {
+  height: 100%;
+  padding: 35px 30px;
+  box-sizing: border-box;
+  overflow: auto;
+}
 </style>
---
+++ src/components/pages/setting/instalment.vue
@@ -0,0 +1,156 @@
+<template>
+  <!-- 分期模板 -->
+  <div class="Instalment">
+    <div class="delete">
+      <p class="add" @click="openDia('新增模板')">新增</p>
+      <p class="shanchu">删除</p>
+    </div>
+
+    <div class="ower-table">
+      <el-table
+        ref="multipleTable"
+        :data="tableData"
+        style="width: 100%"
+        @selection-change="handleSelectionChange"
+        height="580">
+        <el-table-column
+          type="selection"
+          width="55">
+        </el-table-column>
+        <el-table-column
+          prop="date"
+          label="模板名称">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="期数">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="首付款">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="服务费">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="利率"
+          min-width="300">
+        </el-table-column>
+        <el-table-column
+          label="操作">
+          <template slot-scope="scope">
+            <el-button type="text" style="color: #5962FF;" @click="openDia('编辑模板')">编辑</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+
+    <div class="ower-pages" v-if="pages.total > pages.pageSize">
+      <el-pagination
+        background
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="pages.currentPage"
+        :page-sizes="pages.pageSizes"
+        :page-size="pages.pageSize"
+        layout="sizes, prev, pager, next, total"
+        :total="pages.total">
+      </el-pagination>
+    </div>
+
+    <el-dialog
+      :visible.sync="dialogVisible"
+      width="683px">
+      <div class="dialog-header">{{title}}</div>
+      <div class="form">
+        <el-form ref="form" :model="form" label-width="80px">
+          <el-form-item label="活动名称：">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="活动名称：">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="活动名称：">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="活动名称：">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="活动名称：">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+        </el-form>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Instalment',
+  data () {
+    return {
+      value: '',
+      options: [],
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }
+      ],
+      pages: {
+        currentPage: 1,
+        pageSize: 10,
+        pageSizes: [10, 20, 30, 40],
+        total: 10
+      },
+      multipleSelection: [],
+      dialogVisible: false,
+      title: '新增模板',
+      form: {
+        name: ''
+      }
+    }
+  },
+  mounted () {
+    this.pages.total = this.tableData.length
+  },
+  methods: {
+    handleSizeChange (val) {
+      console.log(`每页 ${val} 条`)
+    },
+    handleCurrentChange (val) {
+      console.log(`当前页: ${val}`)
+    },
+    handleSelectionChange (val) {
+      this.multipleSelection = val
+    },
+    openDia (msg) {
+      this.title = msg
+      this.dialogVisible = true
+      if (msg === '新增模板') {
+
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.Instalment {
+  padding: 0 30px 30px 30px;
+  box-sizing: border-box;
+  overflow: auto;
+  height: 100%;
+  .form {
+    padding: 25px 120px 0 90px;
+  }
+}
+</style>
