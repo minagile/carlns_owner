0f6c9626594aad177d693cb14c3f4fd22c227484
XianQijing <154122662@qq.com>
2018-12-25 17:36:48 +0800

    'jjj'

--- src/components/pages/setting/typesOfInsurance.vue
+++ src/components/pages/setting/typesOfInsurance.vue
@@ -295,12 +295,10 @@ export default {
     },
     // 多选删除
     deleteData (id) {
-      let ids = []
       if (!id) {
         this.all.forEach(v => {
-          ids.push(v.billId)
+          id += v.billId + ','
         })
-        id = ids.toString()
       }
       this.$confirm(`此操作将永久删除该信息, 是否继续?`, '提示', {
         confirmButtonText: '确定',
